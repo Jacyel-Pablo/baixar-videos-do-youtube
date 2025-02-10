1c4efb9128155582afda6c4018f697d0b05acb7d
Jacyel-Pablo <jacyell.moura@gmail.com>
2025-02-10 15:00:41 -0300

    Caracteres invalidos são trocados por "" evitando que o arquivo não baixe corretamente também agora a rota só aceita requisições tipo "GET" também foi inserido o try e o except para tratar erros

--- server/server.py
+++ server/server.py
@@ -1,18 +1,33 @@
-from flask import Flask, request, send_file
+from flask import Flask, request, send_file, jsonify
 from pytubefix import YouTube
 from pytubefix.cli import on_progress
+from platform import system
 
 app = Flask(__name__)
 
-@app.route("/download")
+@app.route("/download", methods=["GET"])
 def baixar():
-    url = request.args.get("url")
-    print(url)
+    try:
+        url = request.args.get("url")
+        print(url)
 
-    yt = YouTube(url, on_progress_callback=on_progress)
-    ys = yt.streams.get_highest_resolution()
-    ys.download(output_path="server/static")
+        yt = YouTube(url, on_progress_callback=on_progress)
+        ys = yt.streams.get_highest_resolution()
+        ys.download(output_path="server/static")
 
-    return send_file(f"static/{ys.title}.mp4", as_attachment=True)
+        if system() == "Windows":
+            titulo = ys.title.replace("/", "").replace("\\", "").replace(":", "").replace("*", "").replace("?", "").replace('"', "").replace("<", "").replace(">", "").replace("|", "")
+
+            # Resolvendo problemas com caracteres do windows
+            return send_file(f"static/{titulo}.mp4", as_attachment=True)
+
+        else:
+            titulo = ys.title.replace("/", "")
+
+            # Resolvendo problemas com caracteres do linux
+            return send_file(f"static/{titulo}.mp4", as_attachment=True)
+    
+    except:
+        return jsonify("Ocorreu um erro tenter usar outra url")
 
 app.run(port=3000, host="localhost")
